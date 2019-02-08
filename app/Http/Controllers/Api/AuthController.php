abca6152a28904aa49db48b57c9655eecc7d5c63
hirokinishizawa <hiroki.nishizawa@scouter.co.jp>
2019-02-08 23:30:10 +0900

    requestファイル作成

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Auth\RegisterRequest;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,14 +10,8 @@ use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|unique:users,email|email',
-            'password' => 'required'
-        ]);
-
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rule = [
+            'name' => [
+                'required'
+            ],
+            'email' => [
+                'required|unique:users,email|email'
+            ],
+            'password' => [
+                'required'
+            ]
+        ];
+
+        return $rule;
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必須です。',
+            'email.required' => 'メールアドレスは必須です。',
+            'email.email' => 'メールアドレスを正しい形式にしてください。',
+            'email.unique:users' => 'すでに登録されているアドレスです。',
+            'password.required' => 'パスワードは必須です。'
+        ];
+    }
+}
